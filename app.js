865895806b10506ba814ad94e4b13a98d2023d54
arishaahanif <arishaahanif@gmail.com>
2021-01-14 04:04:35 -0800

    practise git

---
+++ app.js
@@ -0,0 +1,99 @@
+ // QUES:1
+//  var names = prompt("enter your name");
+//  names = names.toUpperCase();
+//  var originalName = ["areesha","ruba","urooj"];
+//       if (names=originalName){
+//          alert("Thanks for your input");
+//      }
+// QUES:02
+    //  userInput = prompt("write a word")
+    // var firstWord = userInput.slice(0,1);
+    // var remWords = userInput.slice(1);
+    // alert("User inputvar:"+userInput + "/n" + "Title case:"+firstWord.toUpperCase()+remWords);
+
+// QUES:3
+// var mobile = prompt("Enter a mobile");
+//  var charsInMobile = mobile.length;
+//  if (charsInMobile > 3) {
+//  mobileAbbrev = mobile.slice(0, 3);
+//  }
+//  console.log(mobileAbbrev)
+
+
+//  QUES:04
+//  var names = prompt("enter your name")
+//  var lastChar = names.charAt(names.length - 1);
+//  console.log(lastChar)
+
+//  QUES:05
+// var pera = "I am pakistani"
+// var a = pera.indexOf("n")
+// console.log(a);
+
+// QUES:07
+// var word = "“The quick brown fox jumps over the lazy dog"
+// var a = word.indexOf("the")
+// console.log(a)
+
+
+ // ques:09
+// var pera = "hyderabad"
+// var pera = pera.replace('hyder','islam')
+// console.log(pera)
+
+// QUES:10
+// var message = "Ali and Sami are best friends. They play cricket and football together."
+// var message = message.replace(/and/g,'&')
+// console.log(message)
+
+// QUES:11
+// var numberOfStars = Math.round(3.45214);
+// console.log(numberOfStars)
+
+// var numberOfStars =  Math.ceil(3.4521);
+// console.log(numberOfStars)
+
+// var numberOfStars =  Math.floor(3.4521);
+// console.log(numberOfStars)
+
+// QUES:12
+
+// var numberOfStars = Math.round(-2.678);
+// console.log(numberOfStars)
+
+// var numberOfStars =  Math.ceil(-2.678);
+// console.log(numberOfStars)
+
+// var numberOfStars =  Math.floor(-2.678);
+// console.log(numberOfStars)
+
+// QUES:13
+// var a = Math.random()
+// console.log(a)
+// var c = a*6
+// console.log(Math.ceil(c))
+
+// var a = Math.random()*3
+//  a = Math.floor(a)
+// console.log(a);
+
+// QUES:14
+// var a = prompt("Enter a secret number")
+// if(a <= 10){
+//     alert("Congratulations")
+// }
+
+// QUES:15
+// var a = "472"
+// var b = a.toString()
+// var b = typeof(a)
+//  console.log(b)
+
+//  var floatingNumString = "472";
+// var num = Number(floatingNumString);
+// console.log(num)
+
+// QUES:16
+// var numberAsNumber = 1234;
+//  var b = numberAsNumber.toString();
+//  console.log(b)
